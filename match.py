02a471c4e53194a7ecc9a6f2c4b4a2a56390d6a3
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2018-06-30 18:45:11 +0900

    2nd commit

--- match.py
+++ match.py
@@ -19,18 +19,48 @@ def one(img, temp):
     cv2.rectangle(img, (pt[0], pt[1]), (pt[0] + w, pt[1] + h), (0,0,200), 1)
     cv2.imwrite("output.png", img)
 
+#ボックスが塗りつぶされているかを確認する関数
+def is_checked(img, left_x, left_y, right_x, right_y):
+    #画素の値を調べる
+    #画像サイズは10x10なので、画素は１００個ある
+    thre = 60
+    black = 0
+    white = 0
+
+    for _x in range(left_x, right_x):
+        if black >= thre:
+            return True
+
+        elif white >= thre:
+            return False
+
+        else:
+            for _y in range(left_y, right_y):
+                #print img[_y][_x]
+                if img[_y][_x] == 0:
+                    black+=1
+
+                elif img[_y][_x] == 255:
+                    white+=1
+
+                else:
+                    pass
+
+
+
+
 def mul(img, temp):
-    template_width = 10
-    template_height = 10
+    template_width = temp.shape[0]
+    template_height = temp.shape[1]
+
 
     box_pt = []
 
     matches = cv2.matchTemplate(img, temp, cv2.TM_CCORR_NORMED)
 
-    threshold = 0.91
+    threshold = 0.90
 
-    print matches
-
+    #tempの探索
     for y in xrange(matches.shape[0]):
         for x in xrange(matches.shape[1]):
             if matches[y][x] > threshold:
@@ -41,17 +71,32 @@ def mul(img, temp):
 
     cv2.imwrite("out.png", img)
 
-    print box_pt
+    #print box_pt
 
-    if len(box_pt) == 4:
-        cv2.rectangle(img, box_pt[0], box_pt[3], (255,0,0), 1)
-
-        new=img[box_pt[0][1]:box_pt[3][1], box_pt[0][0]:box_pt[3][0]]
-
-        cv2.imwrite("output.png", new)
+    #tempは４つしかない
+    if len(box_pt) != 4:
+        print "Can't find marker correctly"
 
     else:
-        print "Can't find marker correctly"
+        cv2.rectangle(img, box_pt[0], box_pt[3], (255,0,0), 1)
+
+        trim = img[box_pt[0][1]:box_pt[3][1], box_pt[0][0]:box_pt[3][0]]
+
+        new = cv2.cvtColor(trim, cv2.COLOR_RGB2GRAY)
+
+        color = (0,0,0) #black
+
+        for i in range(10):
+            for k in range(5):
+                cv2.rectangle(new, (200+20*k, 50+20*i), (210+20*k, 60+20*i), color, 1)
+
+        cv2.rectangle(new, (200, 50), (210, 60), color, -1)
+
+        print is_checked(new, 200, 50, 210, 60)
+        print is_checked(new, 220, 50, 230, 60)
+
+        #print new.shape
+        cv2.imwrite("output.png", new)
 
 def main():
     img = cv2.imread("./format.png")
--- show.py
+++ show.py
@@ -1,54 +1,35 @@
 #!/usr/bin/env python
 #coding: utf-8
-import rospy
 import cv2
-import numpy as np
 
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+def capture_camera(size=None):
+    """Capture video from camera"""
+    # カメラをキャプチャする
+    cap = cv2.VideoCapture(0) # 0はカメラのデバイス番号
 
-
-def img_cb(msg):
-    try:
-        #rospy.loginfo("Subscribed Image Topic !")
-        cv_img = CvBridge().imgmsg_to_cv2(msg, "bgr8")
-        temp = cv2.imread("/home/rg26/catkin_ws/src/omr/img/tmp.png")
-
-        cv_height = cv_img.shape[0]
-        cv_width = cv_img.shape[1]
-
-        template_width = 10
-        template_height = 10
-        threshold = 0.90
-
-        box_pt = []
-
-        matches = cv2.matchTemplate(cv_img, temp, cv2.TM_CCORR_NORMED)
-
-        for y in xrange(matches.shape[0]):
-            for x in xrange(matches.shape[1]):
-                if matches[y][x] > threshold:
-                    cv2.rectangle(cv_img, (x, y),
-                                  (x + template_width, y + template_height),
-                                  (0, 0, 255), 1)
-                    box_pt.append((x+5,y+5))
-
-        cv2.rectangle(cv_img, box_pt[0], box_pt[3], (255,0,0), 1)
-
-        cv2.imshow("window", cv_img)
-        cv2.waitKey(1)
+    while True:
+        # retは画像を取得成功フラグ
+        ret, frame = cap.read()
 
 
-    except CvBridgeError, e:
-        rospy.logerror("Failed to Subscribe Image Topic")
+        # フレームをリサイズ
+        # sizeは例えば(800, 600)
+        if size is not None and len(size) == 2:
+            frame = cv2.resize(frame, size)
+
+        # フレームを表示する
+        cv2.imshow('camera capture', frame)
+
+        k = cv2.waitKey(1) # 1msec待つ
+        if k == 27: # ESCキーで終了
+            break
+
+    # キャプチャを解放する
+    cap.release()
+    cv2.destroyAllWindows()
 
 def main():
-    rospy.init_node("img_subscriber", anonymous=True)
-
-    img_topic_name = "/usb_cam/image_raw"
-
-    rospy.Subscriber(img_topic_name, Image, img_cb)
-    rospy.spin()
+    capture_camera()
 
 if __name__ == "__main__":
     main()
